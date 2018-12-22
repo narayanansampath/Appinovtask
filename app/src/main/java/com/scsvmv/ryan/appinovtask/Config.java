e83427de29451b0b485c9f2927198e43754b3e1a
Narayan <sampathnarayanan72@gmail.com>
2018-12-22 10:31:04 +0530

    added youtube api key

---
+++ app/src/main/java/com/scsvmv/ryan/appinovtask/Config.java
@@ -0,0 +1,10 @@
+package com.scsvmv.ryan.appinovtask;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCJ-8OibFGOGnTAYzI3ktqTm0_WJ9119jg";
+
+}
